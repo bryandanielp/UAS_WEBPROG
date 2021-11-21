2937d26877b34936aa79247daafdb657fea978c6
widi <widialonk101@gmail.com>
2021-11-21 23:07:29 +0700

    update all product

--- seed/seedScript.js
+++ seed/seedScript.js
@@ -7,17 +7,17 @@ const allproducts = [
     new Product({
         imagePath: "https://images.unsplash.com/photo-1625645758520-69e4db363b8d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=871&q=80",
         name: 'PS4 SLIM 500GB',
-        price: 3.000.000
+        price: 3000000
     }),
     new Product({
         imagePath: "https://images.unsplash.com/photo-1544652478-6653e09f18a2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
         name: 'KEYBOARD RGB',
-        price: 1.500.000
+        price: 1500000
     }),
     new Product({
-        imagePath: "https://images.unsplash.com/photo-1552346154-21d32810aba3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c25lYWtlcnN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
-        name: 'Outer Wear',
-        price: 149999
+        imagePath: "https://images.unsplash.com/photo-1616196395385-2587a28072eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        name: 'NINTENDO SWITCH V1',
+        price: 3000000
     }),
     new Product({
         imagePath: "https://images.unsplash.com/photo-1552346154-21d32810aba3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c25lYWtlcnN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
